4226c5c3228cce7ce423c80d24733492ce0f4161
gilboleda <gilboleda@gmail.com>
2022-03-15 13:27:18 +0100

    canvis5

--- src/vad.c
+++ src/vad.c
@@ -95,8 +95,8 @@ VAD_STATE vad(VAD_DATA *vad_data, float *x) {
   switch (vad_data->state) {
   case ST_INIT:
     vad_data->state = ST_SILENCE;
-    vad_data->p1 = f.p + 5; //prova
-    
+    vad_data->p1 = f.p + vad_data->alpha1; //prova
+
     break;
 
   case ST_SILENCE:
